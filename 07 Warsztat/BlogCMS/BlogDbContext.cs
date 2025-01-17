d0e78cf1c7e535c41ae1c08e185c5cbba168b3bb
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2025-01-17 19:39:55 +0100

    BlogCMS workshop

---
+++ 07 Warsztat/BlogCMS/BlogDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogCMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogCMS
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ 07 Warsztat/BlogCMS/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogCMS.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        public Task<IEnumerable<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(int id);
+        public Task<int> AddAsync(T entity);
+        public Task<bool> UpdateAsync(T entity);
+        public Task<bool> DeleteAsync(T entity);
+    }
+}
